ca6877adeede8c3b6f00a428e184099f6f7b0102
小蔡吖 <3081532125@qq.com>
2023-05-31 21:24:45 +0800

    提取方法2

--- src/Print.java
+++ src/Print.java
@@ -1,6 +1,11 @@
 public class Print {
     public static void main(String[] args) {
         fun1();
+        fun2();
+    }
+
+    private static void fun2() {
+        System.out.println("测试提交");
     }
 
     private static void fun1() {
